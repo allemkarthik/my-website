c80d305a5f97c262cbc6d796034384d86f151324
allemkarthik <allemkarthik@gmail.com>
2025-02-01 17:33:00 -0700

    updated my education

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Python Developer",  "full stack web develop"],
+    strings: ["Python Developer",  "full stack web developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
